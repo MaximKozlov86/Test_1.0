a90f20483aa2a09a5154c91598096b15472ae48a
MaximKozlov86 <chupavoin16@rambler.ru>
2024-08-05 22:12:01 +0500

    dz

---
+++ HW10/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    static String booksInfo = "";
+
+
+    public static void main(String[] args) {
+        while (true) {
+            String name = inputBookName();
+            int pageCount = inputPageCount();
+            double bookPrice = inputPagePrice();
+            addBook(name);
+            addBook(name, pageCount);
+            //todo использовать новый метод тут
+            addBook(name, pageCount, bookPrice);
+            printInfo();
+        }
+    }
+
+    private static int inputPagePrice() {
+        System.out.println("Введите цену книги:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static String inputBookName() {
+        System.out.println("Введите название книги:");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static int inputPageCount() {
+        System.out.println("Введите кол-во страниц:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void addBook(String bookName) {
+        addBook(bookName, 0);
+    }
+
+    public static void addBook(String bookName, int pageCount) {
+        addBook(bookName, pageCount, 0);
+    }
+
+    public static void addBook (String bookName, int pageCount, double bookPrice){
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A ") + " стр." + " цена: " + (bookPrice > 0 ? bookPrice : "N/A ") + "р." + "\n";
+    }
+
+    public static void printInfo() {
+        System.out.println(booksInfo);
+    }
+
+}
